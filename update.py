97d6c82c87efc5f305d1082afd4458c68391bbd7
YangNV <smallblack1998@outlook.com>
2019-04-21 04:03:51 +0800

    init commit

--- update.py
+++ update.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     host = "127.0.0.1"
     containers_info = get_container_info()
     for container in containers_info:
-        service_id = buildPayload.build_service(container_info=container, port=9000)
+        service_id = buildPayload.build_service(container_info=container)
         buildPayload.build_route(container_info=container, host=host, id=service_id)
     print("Done")
